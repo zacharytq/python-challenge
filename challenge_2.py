f25be03d70ba2b6d8eefa92c23ab55f21dff024a
Zach Quinn <zacharytq@gmail.com>
2021-10-27 13:50:17 -0400

    challenge 2 done

--- challenge_2.py
+++ challenge_2.py
@@ -1,5 +1,8 @@
 from collections import Counter
 
+n = 2
+
 with open('challenge_2.txt') as challenge_2:
     contents = challenge_2.read()
+    counted = Counter(contents)
     print(Counter(contents))
